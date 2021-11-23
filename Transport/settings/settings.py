0d15cf43fb29eb5b320603477af5287ad8ee805a
ezzimo <ezzi.mohammed1@yahoo.fr>
2021-11-23 06:48:33 +0100

    settings added

--- Transport/settings/settings.py
+++ Transport/settings/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = "Transport.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+django_heroku.settings(locals())
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(conn_max_age=86400),
 }
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 
 
 # Password validation
